925cd2c68dca79c3b6ce99dfee5e37e087ab6008
mccrush <mccrush2027@gmail.com>
2019-04-07 08:37:08 +0700

    Настраивает роутинг

--- src/router.js
+++ src/router.js
@@ -14,14 +14,74 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: function () { 
-        return import(/* webpackChunkName: "about" */ './views/About.vue')
-      }
+      path: '/index.html',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/index.html?',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/alphabet/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/alphabet/?',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/alphabet/index.html',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/alphabet/index.html?',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/ab/index.html',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/ab/index.html?',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/ab/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/app/ab/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/ab/?',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/app/ab/?',
+      name: 'home',
+      component: Home
     }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: function () { 
+    //     return import(/* webpackChunkName: "about" */ './views/About.vue')
+    //   }
+    // }
   ]
 })

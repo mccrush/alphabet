45f3e7beb000a198962ab70dcb5d5786b9ee1405
mccrush <mccrush2027@gmail.com>
2019-04-07 09:12:59 +0700

    Настраивает описание

---
+++ src/components/Modal.vue
@@ -0,0 +1,41 @@
+<template>
+  <!-- Modal -->
+  <div class="modal fade" id="modalWindow" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalCenterTitle">{{local.modalwindow.title}}</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">{{local.modalwindow.message}}</div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="closeModalWindowForeve">{{local.modalwindow.buttoncloseforeve}}</button>
+          <button type="button" class="btn btn-primary" data-dismiss="modal">{{local.modalwindow.buttonclose}}</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    local: Object
+  },
+  methods: {
+    closeModalWindowForeve: function() {
+      localStorage.setItem("showmodal", false);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.modal-header .close:focus {
+  outline: none;
+}
+.modal-content {
+  border: none;
+}
+</style>
--- src/language/en.js
+++ src/language/en.js
@@ -8,10 +8,10 @@ export default {
   },
   modalwindow: {
     title: 'What do we have to do?',
-    message: 'It is necessary to pronounce the colors of words written out loud as soon as possible',
+    message: 'When symbols appear, we pronounce the upper letter out loud and raise the corresponding hand (L-left, P-right, O-both)',
     buttoncloseforeve: 'Close forever',
     buttonclose: 'I understood'
   },
-  descriptionapp: '<small><p>In psychology, <a href="https://www.wikiwand.com/en/Stroop_effect" target="_blank" title="Wikipedia" data-toggle="tooltip" data-placement="bottom">the Stroop effect</a> is a demonstration of interference in the reaction time of a task.</p><p>When the name of a color (e.g., "blue", "green", or "red") is printed in a color which is not denoted by the name (i.e., the word "red" printed in blue ink instead of red ink), naming the color of the word takes longer and is more prone to errors than when the color of the ink matches the name of the color.</p></small>',
+  descriptionapp: '<small><p>This application contributes to the development of skills perception, concentration. It is necessary to say out loud the appearing letter and raise the indicated hand or both hands. At first glance, this may seem like a fairly simple exercise, but in reality everything turns out to be completely different.</p></small>',
   buttonsave: "Save"
 }

88152c073cced2e3490c9cb05efe25233785e8f4
mccrush <mccrush2027@gmail.com>
2019-04-07 08:43:22 +0700

    Добавляет файлы языков

---
+++ src/language/en.js
@@ -0,0 +1,17 @@
+export default {
+  titleapp: 'Alphabet',
+  settings: 'Settings',
+  selectlanguage: {
+    title: 'Language',
+    en: 'En',
+    ru: 'Ru'
+  },
+  modalwindow: {
+    title: 'What do we have to do?',
+    message: 'It is necessary to pronounce the colors of words written out loud as soon as possible',
+    buttoncloseforeve: 'Close forever',
+    buttonclose: 'I understood'
+  },
+  descriptionapp: '<small><p>In psychology, <a href="https://www.wikiwand.com/en/Stroop_effect" target="_blank" title="Wikipedia" data-toggle="tooltip" data-placement="bottom">the Stroop effect</a> is a demonstration of interference in the reaction time of a task.</p><p>When the name of a color (e.g., "blue", "green", or "red") is printed in a color which is not denoted by the name (i.e., the word "red" printed in blue ink instead of red ink), naming the color of the word takes longer and is more prone to errors than when the color of the ink matches the name of the color.</p></small>',
+  buttonsave: "Save"
+}

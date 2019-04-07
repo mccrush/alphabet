a591bfbd6f0f7d9ffae7fbcf009e4e13edc9d74f
mccrush <mccrush2027@gmail.com>
2019-04-07 09:02:33 +0700

    Настраивает Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-4">
+    <div class="row justify-content-center">
+      <div class="col"></div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "home",
+  components: {},
+  props: {
+    local: Object
   }
-}
+};
 </script>

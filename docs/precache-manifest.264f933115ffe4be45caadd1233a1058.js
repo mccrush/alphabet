b98c3dbff010fd84eec28a265723d8bf55028b85
mccrush <mccrush2027@gmail.com>
2019-04-07 17:23:13 +0700

    Собирает проект

---
+++ docs/precache-manifest.264f933115ffe4be45caadd1233a1058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "5903eb9dd7ca54085239",
+    "url": "js/chunk-vendors.d8e6b38d.js"
+  },
+  {
+    "revision": "edbbf9ff507317f74ec6",
+    "url": "js/app.e8dc0e6c.js"
+  },
+  {
+    "revision": "f5e4677832c8af9322aa81bc98bae132",
+    "url": "index.html"
+  },
+  {
+    "revision": "5869a0f7f5ffce0c168ce5e676f6f0d5",
+    "url": "css/main.css"
+  },
+  {
+    "revision": "edbbf9ff507317f74ec6",
+    "url": "css/app.e2643437.css"
+  }
+];
